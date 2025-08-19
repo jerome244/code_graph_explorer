0d48e7c778091e84698bd0a1efe7880d2944804d
jerome244 <10485@holbertonstudents.com>
2025-08-19 08:48:03 +0200

    clean

--- config/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-admin.site.site_header = "My App Admin"
-admin.site.site_title = "My App Admin"
-admin.site.index_title = "Administration"
--- core/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import BaseModel
-from .project import Project
-from .analysis import ProjectAnalysis
-
-__all__ = ["BaseModel", "Project", "ProjectAnalysis"]

1ff6e0b28ee0abcf61858a233790fbd72c271d18
jerome244 <10485@holbertonstudents.com>
2025-09-08 22:20:04 +0200

    rm pico

--- pico/routes.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <Arduino.h>
-#include <WiFi.h>
-
-void sendHeaders(WiFiClient& c, const char* type = "text/html; charset=utf-8");
-void sendIndex(WiFiClient& c);
-void handleFirstLine(const String& first, WiFiClient& client);

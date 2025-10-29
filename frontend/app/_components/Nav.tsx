424f48c57b1a9d4a0f3da0730c5d448f8b3c37f3
jerome244 <10485@holbertonstudents.com>
2025-10-29 15:23:31 +0100

    fix : logo name

--- frontend/app/_components/Nav.tsx
+++ frontend/app/_components/Nav.tsx
@@ -137,7 +137,7 @@ export default function Nav() {
               whiteSpace: "nowrap",
             }}
           >
-            DevGraph
+            CodeGraphExplorer
           </span>
         </Link>
 
